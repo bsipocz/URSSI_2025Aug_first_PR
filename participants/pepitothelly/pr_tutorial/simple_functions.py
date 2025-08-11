22869a2530aea400d22322f5540a44c9a2e11b20
Test User <testuser@Pepitos-MacBook-Pro.local>
2025-08-11 11:49:38 -0800

    Added a new function to check whether a number is a prime number

--- participants/pepitothelly/pr_tutorial/simple_functions.py
+++ participants/pepitothelly/pr_tutorial/simple_functions.py
@@ -10,3 +10,15 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number > 1:
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                return False
+        return True
+
+num = int(input("Type in a number: "))
+print(is_prime(num))

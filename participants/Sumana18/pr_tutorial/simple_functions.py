67c46bec2c17a1ca2aa9902a3075b35e7dac27b8
Sumana Sahoo <sumanasahoo@Sumanas-MacBook-Pro.local>
2025-08-11 13:50:43 -0800

    Funtion for prime numbers added and tested

--- participants/Sumana18/pr_tutorial/simple_functions.py
+++ participants/Sumana18/pr_tutorial/simple_functions.py
@@ -10,3 +10,11 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def is_prime(number):
+    if number <= 1:
+        return False  # Numbers less than or equal to 1 are not prime
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False  # If divisible by any number, it's not prime
+    return True  # If no divisors found, it's prime

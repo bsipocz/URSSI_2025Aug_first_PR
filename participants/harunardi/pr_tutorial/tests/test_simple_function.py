40ba33a2725597348518ae41c78e66f7e1edaed0
Harun Ardiansyah <harunardi13@gmail.com>
2025-08-11 14:14:12 -0800

    Edit some functions and tests

--- participants/harunardi/pr_tutorial/tests/test_simple_function.py
+++ participants/harunardi/pr_tutorial/tests/test_simple_function.py
@@ -1,7 +1,10 @@
 from pr_tutorial.simple_functions import factorial
-
+from pr_tutorial.buggy_functions import angle_to_sexigesimal
 
 def test_factorial_3():
     """Simplest test for one crete case"""
 
     assert factorial(3) == 6
+
+def test_angle():
+    assert angle_to_sexigesimal(15.72,decimal=3)

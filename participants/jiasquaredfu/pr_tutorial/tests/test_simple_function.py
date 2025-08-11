bd76a7daf43b5335321a7e39c42610ee07476efe
jiasquaredfu <jjnaturalist@gmail.com>
2025-08-11 14:01:43 -0800

    fixed bug in buggy_function

--- participants/jiasquaredfu/pr_tutorial/tests/test_simple_function.py
+++ participants/jiasquaredfu/pr_tutorial/tests/test_simple_function.py
@@ -1,7 +1,13 @@
 from pr_tutorial.simple_functions import factorial
+from pr_tutorial.buggy_function import angle_to_sexigesimal
+
+#def test_factorial_3():
+#    """Simplest test for one crete case"""
+
+#    assert factorial(3) == 6
 
 
-def test_factorial_3():
-    """Simplest test for one crete case"""
-
-    assert factorial(3) == 6
+def test_bug():
+	assert angle_to_sexigesimal(60)=="4:0:0.000"
+	assert angle_to_sexigesimal(180)=="12:0:0.000"
+ 

eb3af23b0e620da62513653c192f2ae2baf9c6c9
Monika Anna Tomaszewska <monika.anna.tomaszewska@gmail.com>
2025-08-11 18:03:19 -0400

    Made three changes: (i) function def about output format; (ii) change hour calc; (iii) changed format output

--- participants/Rapsodia86/pr_tutorial/buggy_function.py
+++ participants/Rapsodia86/pr_tutorial/buggy_function.py
@@ -12,7 +12,7 @@ def angle_to_sexigesimal(angle_in_degrees, decimals=3):
 
     Returns
     -------
-    hms_str : str
+    " HH:MM:SS.sss" format
         The sexigesimal string giving the hours, minutes, and seconds of RA for
         the given `angle_in_degrees`
 
@@ -20,7 +20,7 @@ def angle_to_sexigesimal(angle_in_degrees, decimals=3):
     if math.floor(decimals) != decimals:
         raise OSError('decimals should be an integer!')
 
-    hours_num = angle_in_degrees*24/180
+    hours_num = angle_in_degrees * 24/360
     hours = math.floor(hours_num)
 
     min_num = (hours_num - hours)*60
@@ -28,5 +28,6 @@ def angle_to_sexigesimal(angle_in_degrees, decimals=3):
 
     seconds = (min_num - minutes)*60
 
-    format_string = '{}:{}:{:.' + str(decimals) + 'f}'
+    #format_string = '{}:{}:{:.' + str(decimals) + 'f}'
+    format_string = f"{{:02d}}:{{:02d}}:{{:0{3+decimals}.{decimals}f}}"
     return format_string.format(hours, minutes, seconds)

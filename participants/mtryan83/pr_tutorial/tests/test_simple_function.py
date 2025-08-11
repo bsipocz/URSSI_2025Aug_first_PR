547249695025ffd45cae1a194144ca206815dca5
Michael Ryan <mtryan83@gmail.com>
2025-08-11 14:38:47 -0700

    Added some basic tests for is_prime

--- participants/mtryan83/pr_tutorial/tests/test_simple_function.py
+++ participants/mtryan83/pr_tutorial/tests/test_simple_function.py
@@ -1,7 +1,20 @@
-from pr_tutorial.simple_functions import factorial
+from pr_tutorial.simple_functions import factorial,is_prime
 
 
 def test_factorial_3():
     """Simplest test for one crete case"""
 
     assert factorial(3) == 6
+
+
+def test_is_prime():
+    """Testing is_prime function"""
+    assert is_prime(2) # 2 is prime
+    assert not is_prime(4) # 4 is composite
+    assert not is_prime(1) # 1 is neither prime nor composite
+    assert not is_prime(-5) # -5 should probably return an error
+    assert is_prime(47) # 47 is prime (and wasn't used in dev)
+    try:
+        is_prime('uhoh') # handling non-integers?
+    except TypeError:
+        pass

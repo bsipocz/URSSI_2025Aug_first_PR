48d75494cb4ead6ba5cb0a66bc1678b1f809d783
Saad Lahrichi <slxg3@umsystem.edu>
2025-08-11 12:01:37 -0800

    ADD: is_prime function to simple_functions

--- participants/slahrichi/pr_tutorial/simple_functions.py
+++ participants/slahrichi/pr_tutorial/simple_functions.py
@@ -10,3 +10,12 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+
+def is_prime(value):
+    if value < 2:
+        return False
+    for i in range(2, int(value**0.5) + 1):
+        if value % i == 0:
+            return False
+    return True

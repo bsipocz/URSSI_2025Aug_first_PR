75bccf438dd6f359f14064e958b1097c32cfe48f
jiasquaredfu <jjnaturalist@gmail.com>
2025-08-11 11:09:31 -0800

    Improve readME and ad is_prime function to simple_functions.py

--- participants/jiasquaredfu/pr_tutorial/simple_functions.py
+++ participants/jiasquaredfu/pr_tutorial/simple_functions.py
@@ -1,3 +1,4 @@
+import numpy as np 
 def fibonacci(max):
     values = [0, 1]
     while values[-2] + values[-1] < max:
@@ -10,3 +11,12 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+
+def is_prime(int_value):
+	for i in range(0,int(np.sqrt(int_value))):
+		if int_value/i !=0:
+			return False
+		else:
+			return True
+

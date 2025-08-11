672e39d10b8dc5f0db592d312988400d3eb676e3
Awe <rebecca.awe575@topper.wku.edu>
2025-08-11 15:08:41 -0500

    I added a new function for PrimeNumbers

--- participants/RehyBehyCkar01/pr_tutorial/simple_functions.py
+++ participants/RehyBehyCkar01/pr_tutorial/simple_functions.py
@@ -10,3 +10,18 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def is_prime(n):
+	if n<= 1:
+	return False
+	if n<= 3:
+	    return true
+	if n % 2 == 0 or n % 3 == 0:
+	     return false
+	i=5
+	while i * i <= n:
+	    if n % i ==0 or n % (i + 2) == 0:
+		return False
+	    i += 6
+	return True
+	
